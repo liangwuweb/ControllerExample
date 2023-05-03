b35b421989919af8c3e26ee3fff466314e532e12
Liang Wu <liangwuweb@gmail.com>
2023-05-03 16:56:55 -0500

    controller start

---
+++ ControllerExample/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControllerExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("sayhello")]
+        public string methodOne() {
+            return "hello from method one";
+        }
+    
+    }
+}
